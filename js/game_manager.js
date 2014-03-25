86d9d354ea6b510dc90d04cae1fd84a83facbd4f
baffonero <fabiodellerba@gmail.com>
2014-03-25 14:02:23 +0100

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -133,6 +133,7 @@ GameManager.prototype.moveTile = function (tile, cell) {
 // Move tiles on the grid in the specified direction
 GameManager.prototype.move = function (direction) {
   // 0: up, 1: right, 2: down, 3: left
+  alert("cambiato-1");
   var self = this;
 
   if (this.isGameTerminated()) return; // Don't do anything if the game's over
